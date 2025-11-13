9aff11d2aa3572cc5bb9806d5739a73dd7b19489
kkalev <kkalev@gmail.com>
2025-11-13 14:25:16 +0200

    update: delete shell scripts

--- build_docker.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker build . -t dummy-gsis-oauth2-login
--- run_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-PORT=4099
-docker stop dummy-gsis-oauth2-login && docker rm dummy-gsis-oauth2-login
-docker run -d -p $PORT:4099 -v $PWD/:/usr/src/app/ -v $PWD/node_modules:/usr/src/app/node_modules --name=dummy-gsis-oauth2-login dummy-gsis-oauth2-login
